5bda428fe3b653972ae97c011471db4412866a26
Foodie <olaorefouad565@gmail.com>
2019-11-30 07:06:58 +0100

    worked on the create, just and range operator

--- app/src/main/java/dev/iamfoodie/rxjava/MainActivity.java
+++ app/src/main/java/dev/iamfoodie/rxjava/MainActivity.java
@@ -11,6 +11,7 @@ import io.reactivex.Observable;
 import io.reactivex.ObservableEmitter;
 import io.reactivex.ObservableOnSubscribe;
 import io.reactivex.Observer;
+import io.reactivex.Scheduler;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.CompositeDisposable;
 import io.reactivex.disposables.Disposable;
@@ -63,6 +64,8 @@ public class MainActivity extends AppCompatActivity {
         });
 
         createOp();
+        justOp();
+        rangeOp();
 
     }
 
@@ -96,6 +99,64 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onComplete() {
                 Log.d(TAG, "Emission Complete!");
+            }
+        });
+    }
+
+    private void justOp() {
+        Observable<Integer> integerObservable = just(1, 2, 3, 4, 5, 6, 7)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread());
+
+        integerObservable.subscribe(new Observer<Integer>() {
+            @Override
+            public void onSubscribe(Disposable d) {
+                Log.d(TAG, "Just Observable Started!");
+                disposable.add(d);
+            }
+
+            @Override
+            public void onNext(Integer integer) {
+                Log.d(TAG, "Integer: " + integer);
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                Log.e(TAG, "Message: " + e.getMessage());
+            }
+
+            @Override
+            public void onComplete() {
+                Log.d(TAG, "Subscription completed!");
+            }
+        });
+    }
+
+    private void rangeOp() {
+        Observable<Integer> rangeObs = range(1, 20)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread());
+
+        rangeObs.subscribe(new Observer<Integer>() {
+            @Override
+            public void onSubscribe(Disposable d) {
+                Log.d(TAG, "Range Obs started!");
+                disposable.add(d);
+            }
+
+            @Override
+            public void onNext(Integer integer) {
+                Log.d(TAG, "Int: " + integer);
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                Log.e(TAG, "Error: " + e.getMessage());
+            }
+
+            @Override
+            public void onComplete() {
+                Log.d(TAG, "Subscription completed!");
             }
         });
     }
